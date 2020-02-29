f27032d4d2d89e4f278996f380b395450ef1579f
Esref Erkam Polat <esreferkam@Esrefs-MacBook-Pro.local>
2020-02-29 14:17:42 -0500

    add Login

---
+++ src/test/Login.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("Enter UserName:");
+        System.out.println("Enter PassWord:");
+        System.out.println("Logging in!");
+
+    }
+}
